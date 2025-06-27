68f1cba84981a669a96b1c11ea72779c76d09f65
Hilary0405 <hilary.mayemba@gmail.com>
2025-06-27 15:18:08 +0200

    Function to check for lowercase caracters

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,11 +2,13 @@
 
 /**
  * _islower - Checks for lowercase character.
+ * @c: The character to check
+ *
  * Return: Always 0
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c<= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
