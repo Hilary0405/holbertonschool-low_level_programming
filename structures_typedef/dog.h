668cd01866eb6291f98a8be06a233c6c5442029e
Hilary0405 <hilary.mayemba@gmail.com>
2025-07-21 03:13:41 +0200

    Define structure of dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure pour décrire un chien
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
